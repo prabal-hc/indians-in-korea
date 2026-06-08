044b9a1e97435cfc4c3e9661298b43ff9c3a0d0b
prabal-hc <prabalhc@gmail.com>
2026-06-08 11:55:40 +0530

    updated loading issue

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,24 +1,5 @@
 "use client";
 
-import { useState } from "react";
-import IIKLoader from "@/components/Iikloader";
-
 export default function Loading() {
-  const [showLoader, setShowLoader] = useState(() => {
-    if (typeof window === "undefined") return false;
-    return !window.localStorage.getItem("iik-loader-seen");
-  });
-
-  if (!showLoader) return null;
-
-  return (
-    <IIKLoader
-      onComplete={() => {
-        if (typeof window !== "undefined") {
-          window.localStorage.setItem("iik-loader-seen", "true");
-        }
-        setShowLoader(false);
-      }}
-    />
-  );
+  return null;
 }
